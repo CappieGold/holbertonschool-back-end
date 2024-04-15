1cdbe9b61d20ac1f012cc2edf8c414c8ac576544
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-04-15 13:03:13 +0000

    task 2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,19 +7,19 @@ import requests
 import sys
 
 
-def export_tasks_to_json(employee_id):
+def export_tasks_to_json(USER_ID):
     """json format"""
 
     # API endpoints
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{USER_ID}"
     todos_url = f"https://jsonplaceholder.typicode.com/\
-todos?userId={employee_id}"
+todos?userId={USER_ID}"
 
     # Fetch user information
     user_response = requests.get(user_url)
 
     user_data = user_response.json()
-    username = user_data['username']
+    USERNAME = user_data['username']
 
     # Fetch tasks for the user
     todos_response = requests.get(todos_url)
@@ -30,16 +30,16 @@ todos?userId={employee_id}"
     tasks_info = []
     for task in todos_data:
         tasks_info.append({"task": task['title'], "completed":
-                           task['completed'], "username": username})
+                           task['completed'], "username": USERNAME})
 
     # Create JSON structure
-    json_output = {str(employee_id): tasks_info}
+    json_output = {str(USER_ID): tasks_info}
 
     # Write data to JSON file
-    with open(f"{employee_id}.json", 'w') as json_file:
+    with open(f"{USER_ID}.json", 'w') as json_file:
         json.dump(json_output, json_file)
 
 
 if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-    export_tasks_to_json(employee_id)
+    USER_ID = int(sys.argv[1])
+    export_tasks_to_json(USER_ID)
