303de3fae6c1159f83a5852166d47935af1492bd
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-04-15 11:42:17 +0000

    task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Script using API and module requests"""
+
+import requests
+import csv
+import sys
+
+
+def export_tasks_to_csv(USER_ID):
+    """csv"""
+
+    # Define the API endpoints
+    user_url = f"https://jsonplaceholder.typicode.com/users/{USER_ID}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{USER_ID}/todos"
+
+    # Fetch user information
+    user_response = requests.get(user_url)
+
+    user_data = user_response.json()
+    USERNAME = user_data['name']
+
+    # Fetch TODOs for the user
+    todos_response = requests.get(todos_url)
+
+    todos_data = todos_response.json()
+
+    # Define CSV file name
+    csv_file_name = f"{USER_ID}.csv"
+
+    # Create and write to CSV
+    with open(csv_file_name, mode='w', newline='') as file:
+        csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+
+        # Write each task to the CSV
+        for task in todos_data:
+            csv_writer.writerow([USER_ID, USERNAME, task['completed'],
+                                 task['title']])
+
+
+if __name__ == "__main__":
+    USER_ID = int(sys.argv[1])
+    export_tasks_to_csv(USER_ID)
